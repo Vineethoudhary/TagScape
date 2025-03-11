d819643ad1059776e443ef0f9c935f87f79da475
Vineet Choudhary <66504448+Vineethoudhary@users.noreply.github.com>
2025-03-11 15:21:22 +0530

    Delete SQL directory

--- SQL/image.sql
+++
@@ -1,9 +0,0 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone ="+00:00";
-
-CREATE TABLE 'images'(
-    'id' int(11) NOT NULL,
-    'image' longblob NOT NULL,
-    'created' datetime NOT NULL DEFAULT current_timestamp()
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
